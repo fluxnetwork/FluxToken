a35118521d57c2f50d2842b5db8f5ace77ca424d
Stefan Beyer <sbeyer@gmail.com>
2019-02-24 19:44:17 +0100

    openzeppelin version

--- contracts/FluxToken.sol
+++ contracts/FluxToken.sol
@@ -1,56 +1,17 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.4;
 
-import "./StandardToken.sol";
-import "./SafeMath.sol";
-import "./Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 
 /// @title  FluxToken
-contract FluxToken is Ownable, StandardToken {
-  using SafeMath for uint;
+contract FluxToken is ERC20, ERC20Detailed {
+  uint private INITIAL_SUPPLY = 1000000000e18;
 
-  /*
-  *  Token metadata
-  */
-  string constant public name = "Flux Token";
-  string constant public symbol = "FLX";
-  uint8 constant public decimals = 18;
-  uint constant multiplier = 10 ** uint(decimals);
-
-
-  /*
-  * Events
-  */
-  event Burnt(address indexed _burner, uint indexed _num, uint indexed _total_supply);
-
-
-  /*
-  * Public Functions
-  */
-  /// @dev Contract constructor function
-  /// @param _owner Address of owners wallet.
-  /// @param _supply Number of initially provided token
-  constructor(address _owner, uint _supply) public {
-
-    require(_owner != 0x0);
-    totalSupply = _supply;
-    balances[_owner] = _supply;
-    emit Transfer(0x0, _owner, balances[_owner]);
-
+  constructor () public
+    ERC20Detailed("FluxToken", "FLX", 18)
+  {
+    _mint(msg.sender, INITIAL_SUPPLY);
   }
 
-
-  /// @notice Allows `msg.sender` to simply destroy `num` token units. This means the total
-  /// token supply will decrease.
-  /// @dev Allows to destroy token units.
-  /// @param _num Number of token units to burn.
-  function burn(uint _num) public {
-    require(_num > 0);
-    require(balances[msg.sender] >= _num);
-
-    balances[msg.sender] = balances[msg.sender].sub(_num);
-    totalSupply = totalSupply.sub(_num);
-    emit Burnt(msg.sender, _num, totalSupply);
-    emit Transfer(msg.sender, 0x0, _num);
-  }
 }
